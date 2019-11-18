d85ee4d4f2f54564404dab22212e1622d7978206
nichtsen <hobblong@163.com>
2019-11-18 03:56:17 +0000

    2 cpu stable

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ cp keystore/* node1/keystore
 
 # geth --datadir "node1" export MyCoin.bak
 # import pre-build chain
-# geth --datadir "node1" import chains/MyCoin/MyCoin.bak
+geth --datadir "node1" import chains/MyCoin/MyCoin.bak
 
 # JS console, "exit" to quit  
 geth --datadir "node1" --nodiscover --preload "builder.js" console 2>> /dev/null
