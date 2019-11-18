c2a37918e2ea532ccc3309a069345492ee7b4909
nichtsen <hobblong@163.com>
2019-11-18 07:45:58 +0000

    stable preload

--- test.sh
+++ test.sh
@@ -1,5 +1,47 @@
 #!/bin/bash
 
+function catch() {
+	if [ $err -ne 0 ]; then
+		echo "Error:" $err 
+		exit 1
+	fi
+}
+
+function askProceed() {
+	read -p "Continue? [Y/n] " ans
+	case "$ans" in
+	y | Y | "")
+	echo "proceeding ..."
+	next=1
+	;;
+	n | N)
+	echo "skipping..."
+	next=0
+	;;
+	*)
+	echo "invalid response"
+	askProceed
+	;;
+	esac
+}
+
+function getEth() {
+	wget https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.9.7-a718daa6.tar.gz
+	err=$?
+	catch
+	tar -C /usr/local -xzf geth*.gz
+	echo "export PATH=$PATH:/usr/local/geth-linux-amd64-1.9.7-a718daa6" >> /etc/profile
+	export PATH=$PATH:/usr/local/geth-linux-amd64-1.9.7-a718daa6
+}
+
+
+echo "#########################################"
+echo "######        set up Eth           ######"
+echo "#########################################"
+askProceed
+if [ $next -eq 1 ]; then
+        getEth
+fi
 geth --datadir "node1" init chains/MyCoin/genesis.json
 cp keystore/* node1/keystore
 
@@ -8,4 +50,4 @@ cp keystore/* node1/keystore
 geth --datadir "node1" import chains/MyCoin/MyCoin.bak
 
 # JS console, "exit" to quit  
-geth --datadir "node1" --nodiscover --preload "builder.js" console 2>> /dev/null
+geth --datadir "node1" --nodiscover --preload "contracts/MyCoin/deploy.js" console 2>> /dev/null
