654fe598e5f150d260b0329588daba39144e2a35
nichtsen <hobblong@163.com>
2019-11-22 08:56:17 +0000

    discovery mode

--- test.sh
+++ test.sh
@@ -50,7 +50,7 @@ cp keystore/* node1/keystore
 geth --datadir "node1" import chains/MyCoin/MyCoin.bak
 
 # JS console, "exit" to quit  
-geth --datadir "node1" --nodiscover --preload "contracts/MyCoin/deploy.js" console 2>> /dev/null
+geth --datadir "node1" --preload "contracts/MyCoin/deploy.js" console 2>> /dev/null
 
 # Issue 16307 
 # nohup geth --datadir "node1" --mine --miner.threads=1 --etherbase=0x0000000000000000000000000000000000000000 2>> miner.log &
